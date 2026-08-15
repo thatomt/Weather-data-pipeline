2095274bc3b08c9b5014fb0c47cd639e82287584
Thato M <thatomsekele@gmail.com>
2026-08-15 21:44:05 +0200

    getting weather data using api

--- get_weather.py
+++ get_weather.py
@@ -1,1 +1,15 @@
+import requests
+import json
 
+# Cape Town's coordinates
+LATITUDE = -33.9249
+LONGITUDE = 18.4241
+
+url = "https://api.open-meteo.com/v1/forecast"
+params = {
+    "latitude": LATITUDE,
+    "longitude": LONGITUDE,
+    "daily": "temperature_2m_max,temperature_2m_min,precipitation_sum",
+    "timezone": "auto",
+    "past_days": 7,
+}
